d240e76a022ad6a25ebc9d62074a84f58044facd
Songtuan Lin <songtuan.lin@anu.edu.au>
2023-10-13 13:54:51 +1100

    ...

--- main.py
+++ main.py
@@ -10,14 +10,19 @@ from model.plan import *
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     domain_file = "test/domain.pddl"
+    # domain_file = "/home/users/u6162630/Datasets/test-1/domain.pddl"
     task_file = "test/task.pddl"
+    # task_file = "/home/users/u6162630/Datasets/test-1/problem.pddl"
     # plan_file = "test/sas_plan"
     pos_plan_files = ["test/val_plan.{}".format(i) for i in range(1, 5, 1)]
+    # pos_plan_files = ["/home/users/u6162630/Datasets/test-1/val_plan.{}".format(i) for i in range(1, 3)]
     neg_plan_files = ["test/inval_plan.{}".format(i) for i in range(1, 5, 1)]
+    # neg_plan_files = ["/home/users/u6162630/Datasets/test-1/inval_plan.{}".format(i) for i in range(1, 3)]
     domain = Domain(domain_file)
     task = Task(task_file)
     pos_plans = [PositivePlan(pos) for pos in pos_plan_files]
     indices = [1, 5, 7, 1]
+    # indices = [0, 0]
     neg_plan_files = zip(neg_plan_files, indices)
     neg_plans = [NegativePlan(neg, idx) for neg, idx in neg_plan_files]
     plans = pos_plans + neg_plans
--- test/task.pddl
+++ test/task.pddl
@@ -3,7 +3,7 @@
     (:domain grap)
 (:objects
     node-0-0 node-0-1 node-0-2 node-1-0 node-1-1 node-1-2 node-2-0 node-2-1 node-2-2 - coordinate
-    obj-1 obj-2 obj-3 - thing
+    obj-1 obj-2 obj-3 obj-4 - thing
 )
 
 (:init
@@ -36,17 +36,16 @@
     (wide obj-2)
     (wide obj-3)
     (obj-at obj-1 node-0-1)
-    (occupied node-0-1)
     (obj-at obj-2 node-1-0)
-    (occupied node-1-0)
     (obj-at obj-3 node-1-1)
-    (occupied node-1-1)
+    (obj-at obj-4 node-0-0)
     (robot-at node-0-0)
     (empty)
 )
 
 (:goal (and
     ;todo: put the goal condition here
+    (obj-at obj-4 node-0-0)
     (obj-at obj-1 node-0-2)
     (obj-at obj-2 node-1-2)
     (obj-at obj-3 node-0-2)
