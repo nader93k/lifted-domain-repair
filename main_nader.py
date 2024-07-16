4e6fe8351f86e43d489e406ba5e6b42a7d2c1456
Pascal Lauer <lauer@cs.uni-saarland.de>
2024-07-16 15:47:05 +0200

    load functionality

--- main_nader.py
+++ main_nader.py
@@ -1,6 +1,4 @@
 # Import necessary modules and classes
-from repairer import *
-from model.plan import *
 import os
 import argparse
 import pickle
@@ -31,6 +29,7 @@ parser.add_argument("--domain_file", type=str, default="domain.pddl")
 parser.add_argument("--task_file", type=str, default="task.pddl")
 parser.add_argument("--white_plan_file", type=str, default="white_plan.pddl")
 parser.add_argument("--pickl-dump", type=str, default=None)
+parser.add_argument("--pickl-load", type=str, default=None)
 
 parser.add_argument("--output_directory", type=str,
                     help="the output file containing found repairs")
@@ -39,25 +38,35 @@ args = parser.parse_args()
 
 
 if __name__ == '__main__':
-    domain_file = os.path.join(args.input_directory
-                               , args.domain_file)
-    task_file = os.path.join(args.input_directory
-                             , args.task_file)
-    white_plan_file = os.path.join(args.input_directory
-                                   , args.white_plan_file)
-    out_file = os.path.join(args.output_directory
-                            , "repairs")
+    if args.pickl_load:
+        with open(args.pickl_load, 'rb') as file:
+            repairer = pickle.load(file)
 
-    domain = Domain(domain_file)
-    task = Task(task_file)
-    white_plan_list = [PositivePlan(white_plan_file)]
+        print('loaded', repairer)
 
-    repairer = Repairer(
-        domain
-        , [(task, white_plan_list)]
-    )
+    else:
+        from repairer import *
+        from model.plan import *
 
-    if args.pickl_dump:
-        save_variable_to_folder(args.pickl_dump, repairer, args.input_directory.split("/")[-1])
+        domain_file = os.path.join(args.input_directory
+                                   , args.domain_file)
+        task_file = os.path.join(args.input_directory
+                                 , args.task_file)
+        white_plan_file = os.path.join(args.input_directory
+                                       , args.white_plan_file)
+        out_file = os.path.join(args.output_directory
+                                , "repairs")
+
+        domain = Domain(domain_file)
+        task = Task(task_file)
+        white_plan_list = [PositivePlan(white_plan_file)]
+
+        repairer = Repairer(
+            domain
+            , [(task, white_plan_list)]
+        )
+
+        if args.pickl_dump:
+            save_variable_to_folder(args.pickl_dump, repairer, args.input_directory.split("/")[-1])
 
     repairer.write(out_file)
