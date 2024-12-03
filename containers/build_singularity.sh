ce6cb5ddc73afd320192c8b57124b426c5e807b3
nader <nader.karimi.b@gmail.com>
2024-12-03 06:04:38 +0000

    container added

---
+++ containers/build_singularity.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# development
+# sudo singularity build --sandbox nader-ijcai singularity.def
+# sudo singularity shell --writable nader-ijcai
+
+# Production
+# sudo singularity build --no-cleanup ijcai-os-python.sif ijcai-os-python.def
+# sudo singularity build --no-cleanup ijcai-nader.sif ijcai-nader.def
