2ca4da0cd262159fec5fc4c67fc59bffe76eebce
nader <nader.karimi.b@gmail.com>
2024-12-29 16:44:18 +1100

    grounder ehnaced a lot - partially lifted actions supported now

--- new_grounder.py
+++
@@ -1,9 +0,0 @@
-from lifted_pddl import Parser
-
-
-parser = Parser()
-parser.parse_domain('./debug_data_2/mprime/domain-pprob31-err-rate-0-5.pddl')
-
-# Parse logistics problem
-parser.parse_problem('./debug_data_2/mprime/pprob31-err-rate-0-5.pddl')
-print("\nApplicable actions in PDDL format:\n", parser.encode_ground_actions_as_pddl(parser.get_applicable_actions(), 'str'))
--- tests/lifted_pddl_grounder/test_lifted_pddl_grounder.py
+++ tests/lifted_pddl_grounder/test_lifted_pddl_grounder.py
@@ -4,6 +4,8 @@ import os
 import unittest
 from model.plan import Domain, Task
 from search_partial_grounding.lifted_pddl_grounder import ground_pddl
+from search_partial_grounding.action_grounding_tools import smart_grounder
+import time
 
 
 # python3 tests/lifted_pddl_grounder/test_lifted_pddl_grounder.py 
@@ -11,10 +13,10 @@ class TestPDDLGrounder():
     def setUp(self):
         base = './tests/lifted_pddl_grounder/test_files/'
     
-        # domain_path = base + "mprime/domain-pprob31-err-rate-0-5.pddl"
-        # task_path = base + "mprime/pprob31-err-rate-0-5.pddl"
-        domain_path = base + "miconic/domain-ps1-0-err-rate-0-1.pddl"
-        task_path = base + "miconic/ps1-0-err-rate-0-1.pddl"
+        domain_path = base + "mprime/domain-pprob31-err-rate-0-5.pddl"
+        task_path = base + "mprime/pprob31-err-rate-0-5.pddl"
+        # domain_path = base + "miconic/domain-ps1-0-err-rate-0-1.pddl"
+        # task_path = base + "miconic/ps1-0-err-rate-0-1.pddl"
 
         with open(domain_path, 'r') as f:
             file_content = f.read()
@@ -27,7 +29,7 @@ class TestPDDLGrounder():
             # print(self.task.to_pddl())
 
     def test_ground_simple_problem(self):
-        result = ground_pddl(self.domain, self.task, "up", "test1")
+        result = ground_pddl(self.domain, self.task, "(feast ?love ?muffin ?learning ?understanding ?chocolate)")
         
         # # Basic checks that grounding worked
         # self.assertIsNotNone(result)
@@ -35,9 +37,26 @@ class TestPDDLGrounder():
 
         print("\nGrounding output:")
         print(result)
+    
+    # def test_smart_grounder(self):
+        
+    #     smart_grounder(self.domain, self.task, "up")
+
+    #     print("\nGrounding output:")
+    #     print(result)
 
 
 if __name__ == '__main__':
     t = TestPDDLGrounder()
     t.setUp()
+
+    start_time = time.time()
+
     t.test_ground_simple_problem()
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Elapsed time: {elapsed_time:.4f} seconds")
+
+
+    # t.test_smart_grounder()
