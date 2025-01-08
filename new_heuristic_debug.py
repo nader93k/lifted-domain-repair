0e3a94f37fd62a7e3a8ed388847605337a23a603
nader <nader.karimi.b@gmail.com>
2025-01-08 14:25:22 +1100

    small fix

--- new_heuristic_debug.py
+++ new_heuristic_debug.py
@@ -7,6 +7,7 @@ from model.plan import *
 from repairer import *
 from exptools import list_instances, smart_instance_generator
 from heuristic_tools import Heurisitc as new_heuristic
+import pickle
 
 
 
@@ -15,20 +16,40 @@ benchmark_path = './input/benchmarks-G1'
 instance_list = list_instances(benchmark_path, None, [], lift_prob=1.0)
 
 # to run on an specific instance, you can pass instance_id like this:
-# instance_list = list_instances(
-#     benchmark_path,
-#     None, 
-#     ['floortile-opt11-strips__popt-p02-003-err-rate-0-5', 'miconic__ps17-3-err-rate-0-3'],
-#     lift_prob=1.0)
+instance_list = list_instances(
+    benchmark_path,
+    None, 
+    ['mprime__pprob25-err-rate-0-1'],
+    lift_prob=1.0)
 with open(os.path.join(os.path.dirname(__file__), 'debug_log.txt'), 'w') as debug_log:
     for instance in smart_instance_generator(
         instance_list, min_length=1,
         max_length=15,
         order='random'):
         instance.load_to_memory()
-        domain = instance.planning_domain
-        task = instance.planning_task
-        plan = instance.lifted_plan
+
+
+
+        # domain = instance.planning_domain
+        # task = instance.planning_task
+        # plan = instance.lifted_plan
+
+        # Read actions.pkl
+        with open('actions.pkl', 'rb') as f:
+            plan = pickle.load(f)
+        print("Actions:", plan)
+
+        # Read domain.pkl
+        with open('domain.pkl', 'rb') as f:
+            domain = pickle.load(f)
+        print("\nDomain:", domain.to_pddl())
+
+        # Read task.pkl
+        with open('task.pkl', 'rb') as f:
+            task = pickle.load(f)
+        print("\nTask:", task.to_pddl())
+
+
 
         base_folder=f''
         # print a headline
