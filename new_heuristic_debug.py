5139d4fdf6cc5facf83f9cfae3eccbc3230b3ded
nader <nader.karimi.b@gmail.com>
2025-01-09 16:52:33 +1100

    new heuristic debug: comparing relaxation effect

--- new_heuristic_debug.py
+++ new_heuristic_debug.py
@@ -9,7 +9,7 @@ import traceback
 from model.plan import *
 from repairer import *
 from exptools import list_instances, smart_instance_generator
-from heuristic_tools import Heurisitc as new_heuristic
+from heuristic_tools import Heurisitc as heuristic
 import pickle
 
 
@@ -19,12 +19,6 @@ benchmark_path = './input/benchmarks-G1'
 instance_list = list_instances(benchmark_path, None, [], lift_prob=1.0)
 debug_data_folder = '/home/remote/u7899572/lifted-white-plan-domain-repair/debug/debug_h_data/'
 
-# to run on an specific instance, you can pass instance_id like this:
-instance_list = list_instances(
-    benchmark_path,
-    None, 
-    ['mprime__pprob25-err-rate-0-1'],
-    lift_prob=1.0)
 with open(os.path.join(os.path.dirname(__file__), 'debug_log.txt'), 'w') as debug_log:
     for instance in smart_instance_generator(
         instance_list, min_length=1,
@@ -32,48 +26,33 @@ with open(os.path.join(os.path.dirname(__file__), 'debug_log.txt'), 'w') as debu
         order='random'):
         instance.load_to_memory()
 
-        # domain = instance.planning_domain
-        # task = instance.planning_task
-        # plan = instance.lifted_plan
-
-        # Read actions.pkl
-        with open(debug_data_folder+'actions.pkl', 'rb') as f:
-            plan = pickle.load(f)
-        print("Actions:", plan)
-
-        # Read domain.pkl
-        with open(debug_data_folder+'domain.pkl', 'rb') as f:
-            domain = pickle.load(f)
-        print("\nDomain:", domain.to_pddl())
-
-        # Read task.pkl
-        with open(debug_data_folder+'task.pkl', 'rb') as f:
-            task = pickle.load(f)
-        print("\nTask:", task.to_pddl())
-
-
+        domain = instance.planning_domain
+        task = instance.planning_task
+        plan = instance.lifted_plan
 
         base_folder=f''
         # print a headline
         print("="*15 + f' INSTANCE ID = {instance.identifier}' + "="*15, file=debug_log)
         print(f'Plan length = {instance.plan_length}', file=debug_log)
-        try:
-            start_time = time.time()
-            h_new = new_heuristic(h_name="L_HADD", relaxation='none')
-            h_cost_new = h_new.evaluate(domain, task, plan)
-            end_time = time.time()
-            wall_time = end_time - start_time
-                # print(f"h_new={h_cost_new}", file=debug_log)
-                # print(f"Wall time - RAM disk: {wall_time:.3f} seconds", file=debug_log)
+        # try:
+        # no relaxation
+        start_time = time.time()
+        no_relax = heuristic(h_name="L_HADD", relaxation='none')
+        h_no_relax = no_relax.evaluate(domain, task, plan)
+        end_time = time.time()
+        wall_time = end_time - start_time
+        print(f"no_relaxation={h_no_relax}", file=debug_log)
+        print(f"Wall time: {wall_time:.3f} seconds", file=debug_log)
 
-        except Exception as e:
-            end_time = time.time()
-            wall_time = end_time - start_time
-            print("="*15 + f' Error in INSTANCE ID = {instance.identifier}' + "="*15, file=debug_log)
-            print(f'Plan length = {instance.plan_length}', file=debug_log)
-            print(f"Wall time: {wall_time:.3f} seconds", file=debug_log)
-            print(f"Failed to process instance {instance.identifier}: {str(e)}", file=debug_log)
-            print("\nFull traceback:", file=debug_log)
-            traceback.print_exc(file=debug_log)
-            print("="*40, file=debug_log)
+        # zeroary
+        start_time = time.time()
+        zeroary = heuristic(h_name="L_HADD", relaxation='zeroary')
+        h_zeroary = zeroary.evaluate(domain, task, plan)
+        end_time = time.time()
+        wall_time = end_time - start_time
+        print(f"zeroary={h_zeroary}", file=debug_log)
+        print(f"Wall time: {wall_time:.3f} seconds", file=debug_log)
+
+        print(f"**diff: norelax - zeroary = {h_no_relax - h_zeroary}", file=debug_log)
+
         debug_log.flush()
