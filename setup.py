263e8404fc2830b91dbd75e18f481f3e7dae0d77
Nader <nader.karimi.b@gmail.com>
2025-08-26 19:13:19 +1000

    removed more files

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="lifted-white-plan-domain-repair",
-    version="0.1",
-    packages=find_packages(exclude=["tests*", "debug*", "exp_logs*", "documentation*"]),
-)
